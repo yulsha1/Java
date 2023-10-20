f7f9fcb50fe3319695fd4f8361dab4b380b49f71
yulsha1 <yaschenkoyl@gmail.com>
2023-10-20 23:00:09 +0300

    Step 2

--- lab1/src/main/java/org/example/StringCalculator.java
+++ lab1/src/main/java/org/example/StringCalculator.java
@@ -3,12 +3,18 @@ package org.example;
 public class StringCalculator {
     int add (String numbers){
         int result=0;
+        String number="";
         for(int i=0;i<numbers.length();i++){
             char charDigit = numbers.charAt(i);
-            if(charDigit==',')continue;
-            int digit = Character.getNumericValue(charDigit);
-            result += digit;
+
+            if(charDigit==','){
+                result+=Integer.parseInt(number);
+                number="";
+                continue;
+            }
+            number+=charDigit;
         }
+        result+=Integer.parseInt(number);
         return result;
     }
 }
