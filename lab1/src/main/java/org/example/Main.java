857f6a0301193a4cbfc8fe8d49b30c8e879005ee
yulsha1 <yaschenkoyl@gmail.com>
2023-10-20 23:36:36 +0300

    Step 3

--- lab1/src/main/java/org/example/Main.java
+++ lab1/src/main/java/org/example/Main.java
@@ -1,15 +1,17 @@
 package org.example;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-         Scanner scanner=new Scanner(System.in);
-         String numbers;
-         while(true){
-        System.out.print("Enter numbers: ");
-        numbers=scanner.nextLine();
-        if(numbers.equals("ex"))return;
-       int result= new StringCalculator().add(numbers);
-        System.out.println("Result: "+result);}
+        Scanner scanner = new Scanner(System.in);
+        String numbers;
+        while (true) {
+            System.out.print("Enter numbers: ");
+            numbers = scanner.nextLine();
+            if (numbers.equals("ex")) return;
+            int result = new StringCalculator().add(numbers);
+            System.out.println("Result: " + result);
+        }
     }
 }
--- lab1/src/main/java/org/example/StringCalculator.java
+++ lab1/src/main/java/org/example/StringCalculator.java
@@ -1,20 +1,35 @@
 package org.example;
 
 public class StringCalculator {
-    int add (String numbers){
-        int result=0;
-        String number="";
-        for(int i=0;i<numbers.length();i++){
+    int add(String numbers) {
+        if(numbers==""){
+            return 0;
+        }
+        int result = 0;
+        String prev="";
+        String number = "";
+        String delimeter = "";
+         numbers=numbers.replace("\\n",",");
+        for (int i = 0; i < numbers.length(); i++) {
             char charDigit = numbers.charAt(i);
 
-            if(charDigit==','){
-                result+=Integer.parseInt(number);
-                number="";
-                continue;
+            if (!Character.isDigit(charDigit)) {
+                System.out.print(delimeter);
+                if (prev.equals(",")) {
+
+                    return 0;
+                }
+                if (charDigit == ',') {
+                    result += Integer.parseInt(number.trim());
+                    prev = ",";
+                    number = "";
+                    continue;
+                }
             }
-            number+=charDigit;
+            number += charDigit;
+            prev=number;
         }
-        result+=Integer.parseInt(number);
+        result += Integer.parseInt(number.trim());
         return result;
     }
 }
