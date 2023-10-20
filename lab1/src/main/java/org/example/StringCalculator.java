538b244557ccf8131a659b0d327c29ad113da0d4
yulsha1 <yaschenkoyl@gmail.com>
2023-10-21 00:14:51 +0300

    Step 6

--- lab1/src/main/java/org/example/StringCalculator.java
+++ lab1/src/main/java/org/example/StringCalculator.java
@@ -2,42 +2,46 @@ package org.example;
 
 public class StringCalculator {
     int add(String numbers) {
-        if(numbers==""){
+        if (numbers == "") {
             return 0;
         }
         int result = 0;
-        String prev="";
+        String prev = "";
         String number = "";
-        String minusNumbers="";
+        String minusNumbers = "";
 
-        if(numbers.startsWith("//")){
-            numbers=numbers.substring(2);
-            char delimeter=numbers.charAt(0);
-            numbers=numbers.substring(1);
-            numbers=numbers.replace(delimeter,',');
+        if (numbers.startsWith("//")) {
+            numbers = numbers.substring(2);
+            char delimeter = numbers.charAt(0);
+            numbers = numbers.substring(1);
+            numbers = numbers.replace(delimeter, ',');
         }
-       numbers= numbers.trim();
-        numbers=numbers.replace("\\n",",");
-        if(numbers.startsWith(",")) {
+        numbers = numbers.trim();
+        numbers = numbers.replace("\\n", ",");
+        if (numbers.startsWith(",")) {
             numbers = numbers.substring(1);
         }
 
         for (int i = 0; i < numbers.length(); i++) {
             char charDigit = numbers.charAt(i);
 
-            if (!Character.isDigit(charDigit)&&charDigit!='-') {
+            if (!Character.isDigit(charDigit) && charDigit != '-') {
                 System.out.println(number);
                 if (prev.equals(",")) {
                     System.out.println("Invalid data");
                     return 0;
                 }
                 if (charDigit == ',') {
-                    int convertNumber=Integer.parseInt(number.trim());
-
-                    if(convertNumber<0){
-                        minusNumbers+=convertNumber+",";
-                        prev=""+convertNumber;
-                        number="";
+                    int convertNumber = Integer.parseInt(number.trim());
+                    if (convertNumber > 1000) {
+                        prev = "" + convertNumber;
+                        number = "";
+                        continue;
+                    }
+                    if (convertNumber < 0) {
+                        minusNumbers += convertNumber + ",";
+                        prev = "" + convertNumber;
+                        number = "";
                         continue;
                     }
                     result += convertNumber;
@@ -47,16 +51,19 @@ public class StringCalculator {
                 }
             }
             number += charDigit;
-            prev=number;
+            prev = number;
         }
-        int convertNumber=Integer.parseInt(number.trim());
-        if(convertNumber<0){
-            minusNumbers+=convertNumber+",";
+        int convertNumber = Integer.parseInt(number.trim());
+        if (convertNumber < 0) {
+            minusNumbers += convertNumber + ",";
 
-        }else{
-        result += convertNumber;}
-        if(minusNumbers!=""){
-            System.out.println("List of minus numbers :"+minusNumbers);
+        } else if (convertNumber > 1000) {
+
+        } else {
+            result += convertNumber;
+        }
+        if (minusNumbers != "") {
+            System.out.println("List of minus numbers :" + minusNumbers);
         }
         return result;
     }
