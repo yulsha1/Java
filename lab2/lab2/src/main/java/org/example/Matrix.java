de4761526d85f18bd925edb3acda07f1f21d5ce3
yulsha1 <yaschenkoyl@gmail.com>
2023-10-23 20:35:53 +0300

    Step 2

--- lab2/lab2/src/main/java/org/example/Matrix.java
+++ lab2/lab2/src/main/java/org/example/Matrix.java
@@ -2,5 +2,34 @@ package org.example;
 
 public class Matrix {
     private int[][] matrix;
+    public Matrix(){
+        matrix=new int[0][0];
+    }
+    public Matrix(int rows, int columns) {
+        matrix = new int[rows][columns];
+    }
+    public Matrix(Matrix matrixToCopy){
+        int rows = matrixToCopy.getRows();
+        int columns = matrixToCopy.getColumns();
+        matrix = new int[rows][columns];
 
+        int[][] sourceMatrix = matrixToCopy.getMatrix();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = sourceMatrix[i][j];
+            }
+        }
+    }
+    
+    public int getRows() {
+        return matrix.length;
+    }
+
+    public int getColumns() {
+        return matrix[0].length;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
